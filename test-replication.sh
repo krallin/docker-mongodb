d6a7aeb13f2b4608920c57c1b70819b217108764
Thomas Orozco <thomas@orozco.fr>
2017-04-06 15:14:25 +0200

    Replace IP hackity hack with docker network
    The IP hack doesn't really work with newer versions of Docker anymore,
    but using a network is simpler / more robust.

--- test-replication.sh
+++ test-replication.sh
@@ -6,19 +6,25 @@ IMG="$1"
 
 R1_CONTAINER="mongodb-r1"
 R1_DATA_CONTAINER="${R1_CONTAINER}-data"
+R1_IP=172.18.0.21
 R1_PORT=27117
 
 R2_CONTAINER="mongodb-r2"
 R2_DATA_CONTAINER="${R2_CONTAINER}-data"
+R2_IP=172.18.0.22
 R2_PORT=27217
 
 R3_CONTAINER="mongodb-r3"
 R3_DATA_CONTAINER="${R3_CONTAINER}-data"
+R3_IP=172.18.0.23
 R3_PORT=27217
+
+NET_NAME="mongonet"
 
 function cleanup {
   echo "Cleaning up"
   docker rm -f "$R1_CONTAINER" "$R1_DATA_CONTAINER" "$R2_CONTAINER" "$R2_DATA_CONTAINER" "$R3_CONTAINER" "$R3_DATA_CONTAINER" || true
+  docker network rm "$NET_NAME" || true
 }
 
 trap cleanup EXIT
@@ -71,10 +77,18 @@ R1_ENV_ARGS+=(
   "-e" "EXPOSE_HOST=$R1_CONTAINER" "-e" "EXPOSE_PORT_${R1_PORT}=${R1_PORT}"
 )
 
+R1_NET_ARGS=(
+  --ip "$R1_IP"
+)
+
 R2_ENV_ARGS+=(
   "${ENV_ARGS[@]}"
   "-e" "PORT=${R2_PORT}"
   "-e" "EXPOSE_HOST=$R2_CONTAINER" "-e" "EXPOSE_PORT_${R2_PORT}=${R2_PORT}"
+)
+
+R2_NET_ARGS=(
+   --ip "$R2_IP"
 )
 
 R3_ENV_ARGS+=(
@@ -83,51 +97,23 @@ R3_ENV_ARGS+=(
   "-e" "EXPOSE_HOST=$R3_CONTAINER" "-e" "EXPOSE_PORT_${R3_PORT}=${R3_PORT}"
 )
 
+R3_NET_ARGS=(
+  --ip "$R3_IP"
+)
+
 echo "Initializing data containers"
 
 for data_container in  "$R1_DATA_CONTAINER" "$R2_DATA_CONTAINER" "$R3_DATA_CONTAINER"; do
   docker create --name "$data_container" "$IMG"
 done
 
-# Now: a hackish hack. We need to get the IP address of our containers so we can add them to
-# --add-host, but there's a chicken and egg problem:
-# + We can only set --add-host at startup.
-# + We can only get a container's IP address after it started.
-# + We have > 1 containers that need to know each other's IP address.
-# So, since we know recent versions of Docker reuse IP addresses, we'll launch as many containers
-# as we need, and get their IP addresses. We expect the same IP addresses will be reused. Ugh.
-
-echo "Guessing IPs"
-
-for container in "$R1_CONTAINER" "$R2_CONTAINER" "$R3_CONTAINER"; do
-  docker run -d --name "$container" "quay.io/aptible/debian:wheezy" sh -c 'hostname -I && exec sleep 10000'
-done
-
-# xargs trims whitespace for us here
-R1_IP="$(docker logs "$R1_CONTAINER" | xargs)"
-R2_IP="$(docker logs "$R2_CONTAINER" | xargs)"
-R3_IP="$(docker logs "$R3_CONTAINER" | xargs)"
-docker kill "$R1_CONTAINER" "$R2_CONTAINER" "$R3_CONTAINER"
-docker rm   "$R1_CONTAINER" "$R2_CONTAINER" "$R3_CONTAINER"
-
 # This is here to emulate functional DNS.
-IP_ARGS=(
+NET_ARGS=(
+  "--net" "$NET_NAME"
   "--add-host" "${R1_CONTAINER}:${R1_IP}"
   "--add-host" "${R2_CONTAINER}:${R2_IP}"
   "--add-host" "${R3_CONTAINER}:${R3_IP}"
 )
-
-function check_ip () {
-  local container="$1"
-  local expected_ip="$2"
-  local real_ip
-  real_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$container")"
-
-  if [[ "$expected_ip" != "$real_ip" ]]; then
-    echo "${container} IP is unexpected: expected ${expected_ip}, got ${real_ip}"
-    exit 1
-  fi
-}
 
 function validate_cluster_conf () {
   local mongo_url="$1"
@@ -137,35 +123,37 @@ function validate_cluster_conf () {
       echo "Some members had an invalid configuration after 5 attempts"
       exit 1
     fi
-    if docker run --rm -i "${IP_ARGS[@]}" "$IMG" --client "$mongo_url" "/tmp/test/assert-votes.js"; then
+    if docker run --rm -i "${NET_ARGS[@]}" "$IMG" --client "$mongo_url" "/tmp/test/assert-votes.js"; then
       break
     fi
     sleep 2
   done
 }
 
+echo "Creating network"
+
+docker network create --subnet=172.18.0.0/16 "$NET_NAME"
 
 echo "Initializing first member"
 
-
 docker run -i --rm \
+  "${R1_NET_ARGS[@]}" \
   "${R1_ENV_ARGS[@]}" \
-  "${IP_ARGS[@]}" \
+  "${NET_ARGS[@]}" \
   --volumes-from "$R1_DATA_CONTAINER" \
   "$IMG" --initialize
 
 docker run -d --name="$R1_CONTAINER" \
+  "${R1_NET_ARGS[@]}" \
   "${R1_ENV_ARGS[@]}" \
-  "${IP_ARGS[@]}" \
+  "${NET_ARGS[@]}" \
   --volumes-from "$R1_DATA_CONTAINER" \
   "$IMG"
-
-check_ip "$R1_CONTAINER" "$R1_IP"
 
 
 # Wait until the first node becomes the master
 R1_URL="$(docker run -i "${R1_ENV_ARGS[@]}" "$IMG" --connection-url | python -c "$READ_DATABASE_URL_SCRIPT")"
-until docker run --rm -i "${IP_ARGS[@]}" "$IMG" --client "$R1_URL" --quiet --eval 'quit(db.isMaster()["ismaster"] ? 0 : 1)'; do sleep 2; done
+until docker run --rm -i "${NET_ARGS[@]}" "$IMG" --client "$R1_URL" --quiet --eval 'quit(db.isMaster()["ismaster"] ? 0 : 1)'; do sleep 2; done
 
 
 echo "Initializing second member"
@@ -173,8 +161,9 @@ R2_URL="$(docker run -i "${R2_ENV_ARGS[@]}" "$IMG" --connection-url | python -c 
 R2_ADMIN_URL="$(docker run -i "${R2_ENV_ARGS[@]}" -e "DATABASE=admin" "$IMG" --connection-url | python -c "$READ_DATABASE_URL_SCRIPT")"
 
 docker run -i --rm \
+  "${R2_NET_ARGS[@]}" \
   "${R2_ENV_ARGS[@]}" \
-  "${IP_ARGS[@]}" \
+  "${NET_ARGS[@]}" \
   --volumes-from "$R2_DATA_CONTAINER" \
   "$IMG" --initialize-from "$R1_URL"
 
@@ -210,15 +199,14 @@ if ! docker logs "$R1_CONTAINER" | grep "${R2_CONTAINER}:${R2_PORT} is now in st
 fi
 
 docker run -d --name "$R2_CONTAINER" \
+  "${R2_NET_ARGS[@]}" \
   "${R2_ENV_ARGS[@]}" \
   "${ENV_ARGS[@]}" \
-  "${IP_ARGS[@]}" \
+  "${NET_ARGS[@]}" \
   --volumes-from "$R2_DATA_CONTAINER" \
   "$IMG"
 
-check_ip "$R2_CONTAINER" "$R2_IP"
-
-until docker run --rm -i "${IP_ARGS[@]}" "$IMG" --client "$R2_ADMIN_URL" --quiet --eval 'quit(db.isMaster()["secondary"] ? 0 : 1)'; do sleep 2; done
+until docker run --rm -i "${NET_ARGS[@]}" "$IMG" --client "$R2_ADMIN_URL" --quiet --eval 'quit(db.isMaster()["secondary"] ? 0 : 1)'; do sleep 2; done
 
 validate_cluster_conf "$R2_ADMIN_URL"
 
@@ -226,27 +214,27 @@ validate_cluster_conf "$R2_ADMIN_URL"
 echo "Initializing third member from second"
 # This will only pass if the initialization script is smart enough to resolve the real primary and not rely on --initialize-from
 docker run -i --rm \
+  "${R3_NET_ARGS[@]}" \
   "${R3_ENV_ARGS[@]}" \
-  "${IP_ARGS[@]}" \
+  "${NET_ARGS[@]}" \
   --volumes-from "$R3_DATA_CONTAINER" \
   "$IMG" --initialize-from "$R2_URL"
 
 docker run -d --name="$R3_CONTAINER" \
+  "${R3_NET_ARGS[@]}" \
   "${R3_ENV_ARGS[@]}" \
-  "${IP_ARGS[@]}" \
+  "${NET_ARGS[@]}" \
   --volumes-from "$R3_DATA_CONTAINER" \
   "$IMG"
 
-check_ip "$R3_CONTAINER" "$R3_IP"
-
 R3_ADMIN_URL="$(docker run -i "${R3_ENV_ARGS[@]}" -e "DATABASE=admin" "$IMG" --connection-url | python -c "$READ_DATABASE_URL_SCRIPT")"
-until docker run --rm -i "${IP_ARGS[@]}" "$IMG" --client "$R3_ADMIN_URL" --quiet --eval 'quit(db.isMaster()["secondary"] ? 0 : 1)'; do sleep 2; done
+until docker run --rm -i "${NET_ARGS[@]}" "$IMG" --client "$R3_ADMIN_URL" --quiet --eval 'quit(db.isMaster()["secondary"] ? 0 : 1)'; do sleep 2; done
 
 # And now, check that our cluster looks healthy!
 validate_cluster_conf "$R3_ADMIN_URL"
 
 echo "Cluster configuration:"
-docker run --rm -i "${IP_ARGS[@]}" "$IMG" --client "$R2_ADMIN_URL" --quiet --eval 'printjson(rs.conf())'
+docker run --rm -i "${NET_ARGS[@]}" "$IMG" --client "$R2_ADMIN_URL" --quiet --eval 'printjson(rs.conf())'
 
 echo "Cluster status:"
-docker run --rm -i "${IP_ARGS[@]}" "$IMG" --client "$R2_ADMIN_URL" --quiet --eval 'printjson(rs.status())'
+docker run --rm -i "${NET_ARGS[@]}" "$IMG" --client "$R2_ADMIN_URL" --quiet --eval 'printjson(rs.status())'
